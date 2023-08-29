96321c95daae974585b10d88c4698606fa891438
Benngki <benediktus.setiawan@binus.ac.id>
2023-08-29 12:27:41 +0700

    first commit

--- model.py
+++
@@ -1,7 +0,0 @@
-import joblib
-
-model = joblib.load('hc_model.pkl')
-encoder = joblib.load('hc_encoder.pkl')
-
-def predict(gender, umur, lingkar_kepala):
-    return encoder.inverse_transform(model.predict([[gender, umur, lingkar_kepala]]))[0]
